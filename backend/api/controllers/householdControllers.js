811fe41d7ba564bb77a48a61385aae8085999215
Abdul Rehman Shoukat <abdulzain19@gmail.com>
2026-04-05 08:26:38 +0100

    added ability to create a household

--- backend/api/controllers/householdControllers.js
+++ backend/api/controllers/householdControllers.js
@@ -1,4 +1,16 @@
 import { Household } from "../../db/models/householdModel.js";
+import crypto from "crypto";
+
+//function to generate a random invitecodes
+const generateInviteCode = (length = 6) => {
+  const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let code = "";
+  const randomBytes = crypto.randomBytes(length);
+  for (let i = 0; i < length; i++) {
+    code += charset[randomBytes[i] % charset.length];
+  }
+  return code;
+};
 
 export const createHousehold = async (req, res) => {
   try {
@@ -12,7 +24,22 @@ export const createHousehold = async (req, res) => {
         .status(400)
         .json({ error: "name field should be a string only" });
     }
-    const newHouse = new Household({ name: name });
+
+    //getting the user id to attach to the household object
+    const user_id = req.user.user._id;
+
+    //invite code generation
+    const code = generateInviteCode(6);
+
+    //household creation
+    const newHouse = new Household(
+      {
+        name: name,
+        createdByUserId: user_id,
+        inviteCode: code
+
+      });
+
     await newHouse.save();
     return res.status(201).json(newHouse);
   } catch (error) {
---
+++ backendPlanning.md
@@ -0,0 +1,2 @@
+Our users can be created and can login now.
+Next, we need the logged in uder to be able to create a household successfully.
